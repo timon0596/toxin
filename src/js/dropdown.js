206e41a35258982e2eda5516d88729416272206e
timon <timon0596@yandex.ru>
2019-05-29 07:39:07 +0300

    dropdown 2.0

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,87 +1,45 @@
-$(".dropdown i").click(function() {
-	$(this).parent().find(".wrapper__dropdown").slideToggle(250);
-	$(this).parent().toggleClass("active");
 
-	let mainNode = $(this).parent();
-	let clear = false;	
-	let control = $(this).parent().find(".control");
-	$(control).each(function(i,elem){
-	$(mainNode).find(".clean").click(function(){
-		$(mainNode).find(".clean").addClass("inactive");
-		clear = true;
-		$(control).each(function(i,el){
-			$(el).find(".number").html(0);
-			$(mainNode).find(".text").html("Сколько гостей");
-		});
-	});
-	let counter =0;
+$(".dropdown").each(function(i,elem){
+	elem.sum=0;
+	$(elem).find("i").click(function(){
+		$(elem).find(".wrapper__dropdown").slideToggle(250)
+	})
+	$(elem).find(".control").each(function(i,el){
+		el.counter=+$(el).find(".number").html()
 
-
-		$(elem).find(".plus").click(function(){
-			let sum =0;
-			if(clear){
-				counter=0;
-				sum=0;
-			}
-			if(counter<0){
-				counter=0;
-				$(elem).find(".number").html(counter);
-			}
-			else{
-				counter=$(elem).find(".number").html();
-				counter++;				
-			}
-
-			
-
-			$(elem).find(".number").html(counter);
-			$(mainNode).find(".clean").removeClass("inactive");
-			$(control).each(function(i,el){
-				sum+=Number($(el).find(".number").html());
-			});
-			
-			if(sum%10==1&&sum!=11)
-				$(mainNode).find(".text").html(String(sum) + " гость");
-			if(sum%10==2||sum%10==3||sum%10==4)
-				$(mainNode).find(".text").html(String(sum) + " гостя");
-			if((sum%10==0&&sum!=0)||sum%10>4||(sum>=11&&sum<=14))
-				$(mainNode).find(".text").html(String(sum) + " гостей");
-			clear=false;
-		});
-
-
-
-		$(elem).find(".minus").click(function(){
-			let sum =0;
-			counter=$(elem).find(".number").html();
-			counter--;
-			if(counter<0){
-				counter=0;
-				$(elem).find(".number").html(counter);
-			}
+		$(el).find(".plus").click(function(){
+			elem.sum=0;
+			el.counter=+$(el).find(".number").html()
+			el.counter++;
+			$(el).find(".number").html(el.counter)
+			$(elem).find(".clean").removeClass("inactive")
+			$(elem).find(".control").each(function(i,el){
+				elem.sum+=+$(el).find(".number").html()
+			})
+		})
+		$(el).find(".minus").click(function(){
+			elem.sum=0;
+			el.counter=+$(el).find(".number").html()
+			if(el.counter<=0)
+				el.counter=0
 			else
-			$(elem).find(".number").html(counter);
-			$(control).each(function(i,el){
-				sum+=Number($(el).find(".number").html());
-			});
+				el.counter--
+			$(el).find(".number").html(el.counter)
+			$(elem).find(".control").each(function(i,el){
+				elem.sum+=+$(el).find(".number").html()
+			})
+			if(elem.sum==0)
+				{
+					$(elem).find(".clean").addClass("inactive")
+				}
 
+		})
 
-
-			if(sum==0){
-					$(mainNode).find(".clean").addClass("inactive");
-					$(mainNode).find(".text").html("Сколько гостей");
-					console.log(sum);
-				}else{
-					if(sum%10==1&&sum!=11)
-						$(mainNode).find(".text").html(String(sum) + " гость");
-					if(sum%10==2||sum%10==3||sum%10==4)
-						$(mainNode).find(".text").html(String(sum) + " гостя");
-					if(sum%10==0||sum%10>4||(sum>=11&&sum<=14))
-						$(mainNode).find(".text").html(String(sum) + " гостей");
-				}		
-		});			
-	});
+	})
+	$(elem).find(".clean").click(function(){
+		$(elem).find(".control").each(function(i,el){
+			$(el).find(".number").html(0)
+		})
+		$(elem).find(".clean").addClass("inactive")
+	})
 });
-	
-
-
