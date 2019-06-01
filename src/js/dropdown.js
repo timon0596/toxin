0a66e8e10f7b19ef840df3adcf219d1d76f570c9
tim <timon0596@yandex.ru>
2019-06-01 13:24:03 +0300

    dropdown.js corrections

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -5,40 +5,64 @@ $(".dropdown").each(function(i,elem){
 		$(elem).find(".wrapper__dropdown").slideToggle(250)
 		$(elem).toggleClass("active")
 	})
-	$(elem).find(".control").each(function(i,el){
+	$(elem).find(".row").each(function(i,el){
 		el.counter=+$(el).find(".number").html()
+
+
+
+
 
 		$(el).find(".plus").click(function(){
 			elem.sum=0;
+			elem.guest=0;
 			el.counter=+$(el).find(".number").html()
 			el.counter++;
 			$(el).find(".number").html(el.counter)
 			$(elem).find(".clean").removeClass("inactive")
-			$(elem).find(".control").each(function(i,el){
+			$(elem).find(".row").each(function(i,el){
 				elem.sum+=+$(el).find(".number").html()
+				if(!$(el).find(".name").hasClass("third"))
+				elem.guest+=+$(el).find(".number").html()
 			})
+			$(elem).find(".text").html(elem.guest)
 		})
+
+
+
+
+
 		$(el).find(".minus").click(function(){
 			elem.sum=0;
+			elem.guest=0;
 			el.counter=+$(el).find(".number").html()
 			if(el.counter<=0)
 				el.counter=0
 			else
 				el.counter--
 			$(el).find(".number").html(el.counter)
-			$(elem).find(".control").each(function(i,el){
+			$(elem).find(".row").each(function(i,el){
 				elem.sum+=+$(el).find(".number").html()
+				if(!$(el).find(".name").hasClass("third"))
+				elem.guest+=+$(el).find(".number").html()
 			})
 			if(elem.sum==0)
 				{
 					$(elem).find(".clean").addClass("inactive")
-				}
-
+				}				
 		})
 
+
+
+
+
 	})
+
+
+
+
+
 	$(elem).find(".clean").click(function(){
-		$(elem).find(".control").each(function(i,el){
+		$(elem).find(".row").each(function(i,el){
 			$(el).find(".number").html(0)
 		})
 		$(elem).find(".clean").addClass("inactive")
