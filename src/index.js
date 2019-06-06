5ef6961824b1d1a5168cd0e33d9ffe0eded0b694
tim <timon0596@yandex.ru>
2019-06-06 08:29:53 +0300

    checkbox buttons finished

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import "./style.sass"
 import "./js/dropdown.js"
 import "./js/dropdown_bedroom.js"
 import "./js/ckbox.js"
+import "./js/ckboxButtons.js"
 console.log('hello world');
---
+++ src/js/ckboxButtons.js
@@ -0,0 +1,8 @@
+$(".ckbox_buttons").find("li").each(function () {
+	$(this).click(function (e) {
+		$(this).find(".ckbox").toggleClass("checked")
+		$(this).find(".check").toggle(250)
+	// body...
+})
+	// body...
+})
