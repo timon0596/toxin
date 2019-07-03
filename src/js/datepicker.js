3db79944d445f2a358d40dfa553f9bc47dc8931c
timon <timon0596@yandex.ru>
2019-07-03 18:16:18 +0300

    datepicker js

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,11 +1,15 @@
 let date=new Date()
 let months=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
+const CW=$(".calendar-wrapper")
+CW["arrival"]=false
+CW["depart"]=false
+CW["arrival-date"]
+CW["depart-date"]
 
 
 
 
-
-function buildCalendar(year,month){
+function buildcalendar(year,month){
 		if(month==12){
 			month=0
 			year++
@@ -19,32 +23,32 @@ function buildCalendar(year,month){
 		let dinm=32-new Date(year,month,32).getDate()
 		if(firstday==0){firstday=7}
 		for(let i=1;i<=dinm;i++){
-		$(".calendar").append(`<div class='number ${months[month]}'></div>`)			
-		$(".calendar .number")[i-1].innerHTML=i			
+		$(".calendar-inner").append(`<div class='number ${months[month]}'></div>`)			
+		$(".calendar-inner .number")[i-1].innerHTML=i			
 		}
 		for(let i=0;i<firstday-1;i++){
-			$(".calendar").prepend("<div class='number prevMonth'></div>")
+			$(".calendar-inner").prepend("<div class='number prevMonth'></div>")
 		}
 		if(month==0)month=12
 		let j=32-new Date(year,month-1,32).getDate()
 		for(let i=firstday-2;i>=0;i--){
-			$(".calendar .prevMonth")[i].innerHTML=j
+			$(".calendar-inner .prevMonth")[i].innerHTML=j
 			j--			
 		}
 		if(month==12)month=0
-		let lngt=$(".calendar .number").length
+		let lngt=$(".calendar-inner .number").length
 		for(let i=0;i<35-lngt;i++){
-			$(".calendar").append("<div class='number nextMonth'></div>")
+			$(".calendar-inner").append("<div class='number nextMonth'></div>")
 		}
-		$(".calendar .nextMonth").each(function(i){
+		$(".calendar-inner .nextMonth").each(function(i){
 			$(this).html(i+1)
 		})
-		if($(".calendar .number").length>35){
-			lngt=$(".calendar .number").length
+		if($(".calendar-inner .number").length>35){
+			lngt=$(".calendar-inner .number").length
 			for(let i=0;i<42-lngt;i++){
-				$(".calendar").append("<div class='number nextMonth'></div>")		
+				$(".calendar-inner").append("<div class='number nextMonth'></div>")		
 			}
-			$(".calendar .nextMonth").each(function(i){
+			$(".calendar-inner .nextMonth").each(function(i){
 				$(this).html(i+1)
 			})
 		}
@@ -52,28 +56,83 @@ function buildCalendar(year,month){
 		
 
 		$(".month").html(months[month] + " " + year)
-		$(".calendar .number").each(function(){
+		$(".calendar-inner .number").each(function(){
 	if($(this).hasClass(months[new Date().getMonth()])){
 		if($(this).html()==new Date().getDate()){
 			$(this).addClass("currentDate")
 		}
 	}
+
+})
+		
+		$(".calendar .number").each(function(){
+			$(this).click(function(){
+				if($(this).hasClass("nextMonth")||$(this).hasClass("prevMonth")){
+					return false
+				}else{
+					
+
+
+					if(CW["arrival"]==true){
+						$(this).parent().find(".arrival").removeClass("arrival")
+						$(this).toggleClass("arrival")
+						CW["arrival-date"]=new Date($(".month").html().split(" ")[1],months.indexOf($(".month").html().split(" ")[0]),$(this).html())
+						CW["arrival-date"]=CW["arrival-date"].toLocaleDateString().split(".")[2]+"-"+CW["arrival-date"].toLocaleDateString().split(".")[1]+"-"+CW["arrival-date"].toLocaleDateString().split(".")[0]
+						$(".date-arrival input").val(CW["arrival-date"])
+						console.log(CW["arrival-date"])
+
+					}
+
+
+
+
+					
+					
+				}
+	})
 })
 	
 }
 
 
 
-buildCalendar(date.getFullYear(),date.getMonth())
+buildcalendar(date.getFullYear(),date.getMonth())
 
-$(".datepicker .next").click(function(){
-	$(".calendar").empty()
-	buildCalendar(date.getFullYear(),date.getMonth()+1)
+$(".calendar .next").click(function(){
+	$(".calendar-inner").empty()
+	buildcalendar(date.getFullYear(),date.getMonth()+1)
 })
-$(".datepicker .prev").click(function(){
-	$(".calendar").empty()
-	buildCalendar(date.getFullYear(),date.getMonth()-1)
+$(".calendar .prev").click(function(){
+	$(".calendar-inner").empty()
+	buildcalendar(date.getFullYear(),date.getMonth()-1)
 })
-console.log()
+
+
+
+$(".date-arrival i").click(function(e){
+	if(CW["depart"]==false){
+		$(".calendar-wrapper").slideToggle(250)
+	if(CW["arrival"]==false){
+		CW["arrival"]=true
+	}else{
+			CW["arrival"]=false
+		}
+	}
+})
+
+
+
+$(".date-depart i").click(function(e){
+	if(CW["arrival"]==false){
+		$(".calendar-wrapper").slideToggle(250)
+	if(CW["depart"]==false){
+		CW["depart"]=true
+	}else{
+			CW["depart"]=false
+		}
+	}	
+})
+
+console.log(CW["arrival"])
 
 
