822b1e4fb7194cf84a5d1bc71ddfe2f74071d7d1
timon <timon0596@yandex.ru>
2019-09-25 03:03:29 +0300

    rich ckbox btns

--- src/js/ckboxButtons.js
+++ src/js/ckboxButtons.js
@@ -1,5 +1,5 @@
-$(".ckbox_buttons").find("li").each(function () {
-	$(this).click(function (e) {
+$(".ckbox_wrapper").each(function () {
+	$(this).parent().click(function (e) {
 		$(this).find(".ckbox").toggleClass("checked")
 		$(this).find(".check").toggle(250)
 	// body...
