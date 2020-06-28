7100615228705c8e348bef8e7cfde32afeb40a70
timon <timon0596@yandex.ru>
2020-06-29 01:49:20 +0300

    expandable checkboxlist

--- src/ui_kit/js/ckb.js
+++ src/ui_kit/js/ckb.js
@@ -1,3 +1,11 @@
 $('.ckb').click(function(){
 	$(this).toggleClass('checked')
 })
+$('.ckblist').each(function(){
+	const i = $(this).find('i')
+	const w = $(this).find('.ckblist__wrapper')
+	i.click(()=>{
+		$(this).toggleClass('expanded')
+		w.slideToggle(250)
+	})
+})
