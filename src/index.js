1f6f8e39887dff75d5dabc006a43cb325c810f09
timon <timon0596@yandex.ru>
2020-07-01 05:29:55 +0300

    btns

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import 'jquery'
+import 'jquery-ui/ui/widgets/slider'
+import 'jquery-ui/themes/base/all.css'
+import 'jquery-ui/themes/base/slider.css'
 import './index.pug'
 import './main.sass'
 import './ui_kit/ui_kit'
--- src/ui_kit/ui_kit.js
+++ src/ui_kit/ui_kit.js
@@ -8,3 +8,19 @@ import './js/ckb'
 $('.dropdown').each((i,el)=>{
 	new Dropdown($(el),i)
 })
+//------------------------slider------------------------
+//------------------------slider------------------------
+//------------------------slider------------------------
+$('.range--slider').each((i,el)=>{
+	$(el).slider({
+      range: true,
+      min: 0,
+      max: 15000,
+      values: [ 5000, 10000 ],
+      slide: function( event, ui ) {
+        $(el).parent().find( ".range--slider__amount" ).text( ui.values[ 0 ] + "₽" + " - " + ui.values[ 1 ] + "₽" );
+      }
+    })
+	$(el).parent().find( ".range--slider__amount" ).text( $( ".range--slider" ).slider( "values", 0 ) + "₽" + " - " +
+	     $( ".range--slider" ).slider( "values", 1 ) + "₽" )
+})
