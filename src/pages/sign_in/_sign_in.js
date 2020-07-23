a651955c49b39d91850834a5a3f46e83e23b80be
timon <timon0596@yandex.ru>
2020-07-23 23:54:13 +0300

    #5

--- src/pages/sign_in/_sign_in.js
+++ src/pages/sign_in/_sign_in.js
@@ -1,19 +1,25 @@
 $(document).ready(()=>{
-	let sign_up=true
-	const $sign_up = $('.block__registration')
-	const $sign_in = $('.block__sign-in')
+	const $sign_up = $('.block-registration')
+	const $sign_in = $('.block-sign-in')
 
-	function signupHandler(){
-		sign_up=!sign_up
-		sign_up?($sign_in.hide(),$sign_up.show()):($sign_up.hide(),$sign_in.show())
+	function signInHandler(){
+		$sign_up.hide()
+		$sign_in.show()
 	}
-	function signinHandler(){
+	function signUpHandler(){
+		$('.nav__btn-item').show()
+		$('.nav__item.nav__user-name').hide()
+		$sign_in.hide()
+		$sign_up.show()
+	}
+	function enterHandler(){
 		$('.nav__btn-item').hide()
-		$('.nav__item.user__name').show()
+		$('.nav__item.nav__user-name').show()
 	}
 	
 	if('/sign_in.html'==location.pathname){
-		$('.js-sign-in,.js-create').click(signupHandler)
-		$('.js-sign-in-btn').click(signinHandler)
+		$('.js-sign-in,.js-nav__sign-in').click(signInHandler)
+		$('.js-btn-create,.js-nav__sign-up').click(signUpHandler)
+		$('.js-sign-in-btn').click(enterHandler)
 	}
 })
