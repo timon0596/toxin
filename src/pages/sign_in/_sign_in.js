efa74d8b806000fc04204d6d1541bd8776981486
tim <timon0596@yandex.ru>
2020-07-29 19:28:52 +0300

    #5 dropdowns header-footer sign-in page

--- src/pages/sign_in/_sign_in.js
+++ src/pages/sign_in/_sign_in.js
@@ -1,20 +1,20 @@
 $(document).ready(()=>{
-	const $sign_up = $('.block-registration')
-	const $sign_in = $('.block-sign-in')
+	const $sign_up = $('.js-block-registration')
+	const $sign_in = $('.js-block-sign-in')
 
 	function signInHandler(){
 		$sign_up.hide()
 		$sign_in.show()
 	}
 	function signUpHandler(){
-		$('.nav__btn-item').show()
-		$('.nav__item.nav__user-name').hide()
+		$('.js-nav__btn-item').show()
+		$('.js-nav__user-name').hide()
 		$sign_in.hide()
 		$sign_up.show()
 	}
 	function enterHandler(){
-		$('.nav__btn-item').hide()
-		$('.nav__item.nav__user-name').show()
+		$('.js-nav__btn-item').hide()
+		$('.js-nav__user-name').show()
 	}
 	
 	if('/sign_in.html'==location.pathname){
