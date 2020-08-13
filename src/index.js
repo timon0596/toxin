1dd8829e435c2e541387bc2c1d41af6e364af76e
timon <timon0596@yandex.ru>
2020-08-13 06:58:26 +0300

    pages/sign_in folder rename

--- src/index.js
+++ src/index.js
@@ -19,29 +19,3 @@ const preview_img = require.context('./img/preview', true, /\.(jpe?g|png|svg)$/)
 const imgs = require.context('./img', false, /\.(jpe?g|png|svg)$/)
 
 
-$(document).ready(()=>{
-	const $sign_up = $('.js-block-registration')
-	const $sign_in = $('.js-block-sign-in')
-
-	function signInHandler(){
-		$sign_up.hide()
-		$sign_in.show()
-	}
-	function signUpHandler(){
-		$('.js-nav__btn-item').show()
-		$('.js-nav__user-name').hide()
-		$sign_in.hide()
-		$sign_up.show()
-	}
-	function enterHandler(){
-		$('.js-nav__btn-item').hide()
-		$('.js-nav__user-name').show()
-	}
-	
-	if('/sign_in.html'==location.pathname){
-		$('.js-block-registration__sign-in-button,.js-nav__sign-in-button').click(signInHandler)
-		$('.js-block-sign-in__button-create,.js-nav__sign-up-button').click(signUpHandler)
-		$('.js-block-sign-in__sign-in-button').click(enterHandler)
-		console.log($('.js-sign-in-btn'))
-	}
-})
