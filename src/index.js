88448f82185771ade9d9d04943638ab11ba4d1b6
timon <timon0596@yandex.ru>
2020-08-13 06:53:01 +0300

    kakaya to neponyatnay hren' c webpackom

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ function importAll (r) {
 import 'jquery'
 import 'jquery-ui/ui/widgets/slider'
 import 'jquery-ui/themes/base/all.css'
-// import 'jquery-ui/themes/base/slider.css'
 import './index.pug'
 import 'air-datepicker/dist/css/datepicker.min.css'
 import 'bootstrap/js/src/carousel.js'
 import './main.sass'
-// import './ui_kit/form_elements/date_dropdown/date_dropdown'
 require.context('./ui_kit/cards', true, /\.scss$/)
 require.context('./ui_kit', true, /\.sass/)
 require.context('./pages', true, /\.sass/)
@@ -20,3 +18,30 @@ const userImg = require.context('./img/users', true, /\.(jpe?g|png)$/)
 const preview_img = require.context('./img/preview', true, /\.(jpe?g|png|svg)$/)
 const imgs = require.context('./img', false, /\.(jpe?g|png|svg)$/)
 
+
+$(document).ready(()=>{
+	const $sign_up = $('.js-block-registration')
+	const $sign_in = $('.js-block-sign-in')
+
+	function signInHandler(){
+		$sign_up.hide()
+		$sign_in.show()
+	}
+	function signUpHandler(){
+		$('.js-nav__btn-item').show()
+		$('.js-nav__user-name').hide()
+		$sign_in.hide()
+		$sign_up.show()
+	}
+	function enterHandler(){
+		$('.js-nav__btn-item').hide()
+		$('.js-nav__user-name').show()
+	}
+	
+	if('/sign_in.html'==location.pathname){
+		$('.js-block-registration__sign-in-button,.js-nav__sign-in-button').click(signInHandler)
+		$('.js-block-sign-in__button-create,.js-nav__sign-up-button').click(signUpHandler)
+		$('.js-block-sign-in__sign-in-button').click(enterHandler)
+		console.log($('.js-sign-in-btn'))
+	}
+})
