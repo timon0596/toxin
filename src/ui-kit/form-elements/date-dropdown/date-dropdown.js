c0cf76349f750fbb666538dea7ca33ddf1b7ba6b
tim <timon0596@yandex.ru>
2020-08-22 11:40:18 +0300

    icon #8

--- src/ui-kit/form-elements/date-dropdown/date-dropdown.js
+++ src/ui-kit/form-elements/date-dropdown/date-dropdown.js
@@ -1,86 +1,94 @@
-import 'air-datepicker'
-import 'jquery-mask-plugin'
-function dateFromLocaleDateString(str){
-	return new Date(str.split('.').reverse().join('.'))
+import "air-datepicker";
+import "jquery-mask-plugin";
+function dateFromLocaleDateString(str) {
+  return new Date(str.split(".").reverse().join("."));
 }
-function dateDropdown(selector){
-	let selectedDates = {fromTo:'',from:'',to:'',days: 0}
-	selectedDates = JSON.parse(localStorage?.getItem('datepicker'))||selectedDates
-	$(selector).each((i,el)=>{
-			const $dateInput = $(el).find('input')
-			$dateInput.mask('00.00.0000')
-			const $datepicker = $(el).find('.date-dropdown__datepicker-wrapper')
-			const DPinstance = $datepicker.datepicker({
-		    minDate: new Date(),
-		    multipleDates: 2,
-		    range: true,
-		    multipleDatesSeparator: ' - ',
-		    dateFormat: !$dateInput[1]?'dd M':'dd.mm.yyyy',
-		    language: {
-		    	daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб']
-		    },
-		    navTitles: {
-		        days: '<div class="heading"><h2>MM yyyy</h2></div>',
-		    },
-		    clearButton: true,
-		    prevHtml:'<div class="icon"><i class="material-icons">arrow_back</i></div>',
-		    nextHtml:'<div class="icon"><i class="material-icons">arrow_forward</i></div>',
-		    onSelect: function(fd, d, picker) {
-		                const options = {year: 'numeric', month: 'numeric', day: 'numeric'}
-		              	selectedDates.fromTo= fd
-		              	selectedDates.from= d[0]?d[0].toLocaleDateString('ru-RU',options):''
-		              	selectedDates.to = d[1]?d[1].toLocaleDateString('ru-RU',options):''
-		              	selectedDates.days = d[1]?Math.round((d[1]-d[0])/1000/60/60/24):0
-		              	let inputCondition = selectedDates.from&&selectedDates.to
-		              	if($dateInput[1]){
-		              		$($dateInput[0]).val(selectedDates.from)
-		              		inputCondition?$($dateInput[1]).val(selectedDates.to):0
-		              	}
-		              	else{
-		              		$dateInput.val(fd)
-		              	}
-		              	const dateSelectEvent = $.Event('new-date-selected')
-		              	dateSelectEvent.selectedDates = {...selectedDates}
-		              	$(el).trigger(dateSelectEvent)
-		            }
-			}).data('datepicker')
-			
-			if(selectedDates.days){
+function dateDropdown(selector) {
+  let selectedDates = { fromTo: "", from: "", to: "", days: 0 };
+  selectedDates =
+    JSON.parse(localStorage?.getItem("datepicker")) || selectedDates;
+  $(selector).each((i, el) => {
+    const $dateInput = $(el).find("input");
+    $dateInput.mask("00.00.0000");
+    const $datepicker = $(el).find(".date-dropdown__datepicker-wrapper");
+    const DPinstance = $datepicker
+      .datepicker({
+        minDate: new Date(),
+        multipleDates: 2,
+        range: true,
+        multipleDatesSeparator: " - ",
+        dateFormat: !$dateInput[1] ? "dd M" : "dd.mm.yyyy",
+        language: {
+          daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        },
+        navTitles: {
+          days: '<div class="heading"><h2>MM yyyy</h2></div>',
+        },
+        clearButton: true,
+        prevHtml: '<i class="icon">arrow_back</i>',
+        nextHtml: '<i class="icon">arrow_forward</i>',
+        onSelect: function (fd, d, picker) {
+          const options = { year: "numeric", month: "numeric", day: "numeric" };
+          selectedDates.fromTo = fd;
+          selectedDates.from = d[0]
+            ? d[0].toLocaleDateString("ru-RU", options)
+            : "";
+          selectedDates.to = d[1]
+            ? d[1].toLocaleDateString("ru-RU", options)
+            : "";
+          selectedDates.days = d[1]
+            ? Math.round((d[1] - d[0]) / 1000 / 60 / 60 / 24)
+            : 0;
+          let inputCondition = selectedDates.from && selectedDates.to;
+          if ($dateInput[1]) {
+            $($dateInput[0]).val(selectedDates.from);
+            inputCondition ? $($dateInput[1]).val(selectedDates.to) : 0;
+          } else {
+            $dateInput.val(fd);
+          }
+          const dateSelectEvent = $.Event("new-date-selected");
+          dateSelectEvent.selectedDates = { ...selectedDates };
+          $(el).trigger(dateSelectEvent);
+        },
+      })
+      .data("datepicker");
 
-				DPinstance.selectDate([dateFromLocaleDateString(selectedDates.from),dateFromLocaleDateString(selectedDates.to)])
+    if (selectedDates.days) {
+      DPinstance.selectDate([
+        dateFromLocaleDateString(selectedDates.from),
+        dateFromLocaleDateString(selectedDates.to),
+      ]);
+    }
 
-			}
-
-			const dpClear = $('.datepicker--button[data-action="clear"]').hide()
-			const dpInline = $(el).find('.datepicker-inline').hide()
-			const dpBtns = $(el).find('.datepicker--buttons')
-			const dpExpand = $dateInput.next('.icon').find('i')
-			console.log(dpExpand)
-			dpExpand.click(()=>{
-				dpInline.slideToggle(250)
-			})
-			const clear = $('<div>',{
-				class: 'button button_grey',
-				html: '<div class="heading"><h3>очистить</h3></div>',
-				on: {
-					click:()=>{
-						dpClear.click()
-						localStorage?.removeItem('datepicker')
-					}
-				}
-			})
-			const apply = $('<div>',{
-				class: 'button button_with-no-bg',
-				html: '<div class="heading"><h3>применить</h3></div>',
-				on: {
-					click:()=>{
-						localStorage.setItem('datepicker',JSON.stringify(selectedDates))
-						dpInline.slideUp(250)
-					}
-				}
-			})
-			dpBtns.append(clear).append(apply)
-	})
+    const dpClear = $('.datepicker--button[data-action="clear"]').hide();
+    const dpInline = $(el).find(".datepicker-inline").hide();
+    const dpBtns = $(el).find(".datepicker--buttons");
+    const dpExpand = $(el).find(".text-field__wrapper .icon");
+    console.log(dpExpand);
+    dpExpand.click(() => {
+      dpInline.slideToggle(250);
+    });
+    const clear = $("<div>", {
+      class: "button button_grey",
+      html: '<div class="heading"><h3>очистить</h3></div>',
+      on: {
+        click: () => {
+          dpClear.click();
+          localStorage?.removeItem("datepicker");
+        },
+      },
+    });
+    const apply = $("<div>", {
+      class: "button button_with-no-bg",
+      html: '<div class="heading"><h3>применить</h3></div>',
+      on: {
+        click: () => {
+          localStorage.setItem("datepicker", JSON.stringify(selectedDates));
+          dpInline.slideUp(250);
+        },
+      },
+    });
+    dpBtns.append(clear).append(apply);
+  });
 }
-dateDropdown('.js-date-dropdown')
-dateDropdown('.js-filter-date-dropdown')
+dateDropdown(".js-date-dropdown");
--- src/ui-kit/form-elements/dropdown/dropdown.js
+++ src/ui-kit/form-elements/dropdown/dropdown.js
@@ -1,119 +1,131 @@
-export class Dropdown{
+export class Dropdown {
 	static declensions = {
 		bed: [
-			['спальня','спальни','спален'],
-			['кровать','кровати','кроватей'],
-			['ванная','ванных','ванных']
+			["спальня", "спальни", "спален"],
+			["кровать", "кровати", "кроватей"],
+			["ванная", "ванных", "ванных"],
 		],
 		guest: [
-			['гость','гостя','гостей'],
-			['младенец','младенца','младенцев']
-		]
+			["гость", "гостя", "гостей"],
+			["младенец", "младенца", "младенцев"],
+		],
+	};
+	constructor(mainDiv, index) {
+		this.index = index;
+		this.$mainDiv = mainDiv;
+		this.$display = this.$mainDiv.find(".dropdown__display");
+		this.$expand = this.$mainDiv.find(
+			".dropdown__body .dropdown__icon-wrapper"
+		);
+		this.$body = this.$mainDiv.find(".dropdown__body");
+		this.$menu = this.$mainDiv.find(".dropdown__menu");
+		this.$clear = this.$mainDiv.find(".dropdown__clear");
+		this.$apply = this.$mainDiv.find(".dropdown__apply");
+		this.$counters = this.$mainDiv.find(".counter");
+		this.$minusButtons = this.$counters.find(".counter__minus");
+		this.$plusButtons = this.$counters.find(".counter__plus");
+		this.$values = this.$counters.find(".counter__value");
+		this.isGuest = this.$mainDiv.hasClass("dropdown_guest");
+		this.dec = Dropdown.declensions[this.isGuest ? "guest" : "bed"];
+		this.localStorageName = this.$mainDiv.attr("class").replace(/\s/g, "");
+		this.values = this.localStorageValues();
+		this.init();
 	}
-	constructor(mainDiv,index){
-		this.index=index
-		this.$mainDiv=mainDiv
-		this.$display=this.$mainDiv.find('.dropdown__display')
-		this.$expand=this.$mainDiv.find('.dropdown__body>.icon')
-		this.$body=this.$mainDiv.find('.dropdown__body')
-		this.$menu=this.$mainDiv.find('.dropdown__menu')
-		this.$clear=this.$mainDiv.find('.dropdown__clear')
-		this.$apply=this.$mainDiv.find('.dropdown__apply')
-		this.$counters=this.$mainDiv.find('.counter')
-		this.$minusButtons=this.$counters.find('.counter__minus')
-		this.$plusButtons=this.$counters.find('.counter__plus')
-		this.$values=this.$counters.find('.counter__value')
-		this.isGuest=this.$mainDiv.hasClass('dropdown_guest')
-		this.dec = Dropdown.declensions[this.isGuest?'guest':'bed']
-		this.localStorageName = this.$mainDiv.attr('class').replace(/\s/g,'')
-		this.values=this.localStorageValues()
-		this.init()
+	localStorageValues() {
+		const vals = localStorage.getItem(this.localStorageName);
+		return vals
+			? JSON.parse(vals)
+			: this.$values
+					.text()
+					.split("")
+					.map((n) => +n);
 	}
-	localStorageValues(){
-		const vals = localStorage.getItem(this.localStorageName)
-		return vals?JSON.parse(vals):this.$values.text().split('').map(n=>+n)
-	}
-	displayValuePart(i){
-		if(this.isGuest){
-			if(i==0){
-				return (this.values[0]+this.values[1])?`${this.values[0]+this.values[1]} ${this.dec[0][this.modulo((this.values[0]+this.values[1])%10)]}, `:''
+	displayValuePart(i) {
+		if (this.isGuest) {
+			if (i == 0) {
+				return this.values[0] + this.values[1]
+					? `${this.values[0] + this.values[1]} ${
+							this.dec[0][this.modulo((this.values[0] + this.values[1]) % 10)]
+					  }, `
+					: "";
+			} else {
+				return this.values[2]
+					? `${this.values[2]} ${
+							this.dec[1][this.modulo(this.values[2]) % 10]
+					  }, `
+					: "";
 			}
-			else{
-				return (this.values[2])?`${this.values[2]} ${this.dec[1][this.modulo(this.values[2])%10]}, `:''	
-			}
-		}
-		else{
-			return (this.values[i])?`${this.values[i]} ${this.dec[i][this.modulo(this.values[i])%10]}, `:''	
-		}		
-	}
-	displayValue(){
-		if(this.isGuest){
-			return `${this.displayValuePart(0)}`+
-			`${this.displayValuePart(2)}`
-		}
-		else{
-			return `${this.displayValuePart(0)}`+
-			`${this.displayValuePart(1)}`+
-			`${this.displayValuePart(2)}`
+		} else {
+			return this.values[i]
+				? `${this.values[i]} ${this.dec[i][this.modulo(this.values[i]) % 10]}, `
+				: "";
 		}
 	}
-	expand(){
-		this.$menu.slideToggle(250)
-		this.$body.toggleClass('dropdown__body_active')
+	displayValue() {
+		if (this.isGuest) {
+			return `${this.displayValuePart(0)}` + `${this.displayValuePart(2)}`;
+		} else {
+			return (
+				`${this.displayValuePart(0)}` +
+				`${this.displayValuePart(1)}` +
+				`${this.displayValuePart(2)}`
+			);
+		}
 	}
-	minus(e){
-		const i = [...this.$minusButtons].indexOf(e.target)
-		this.values[i]--
-		this.values[i]=this.values[i]<0?0:this.values[i]
-		this.render()
+	expand() {
+		this.$menu.slideToggle(250);
+		this.$body.toggleClass("dropdown__body_active");
 	}
-	plus(e){
-		const i = [...this.$plusButtons].indexOf(e.target)
-		this.values[i]++
-		this.render()
+	minus(e) {
+		const i = [...this.$minusButtons].indexOf(e.target);
+		this.values[i]--;
+		this.values[i] = this.values[i] < 0 ? 0 : this.values[i];
+		this.render();
 	}
-	disableOrEnableMinusButton(i){
-		this.values[i]==0?
-			$(this.$minusButtons[i]).addClass('counter__button_disabled'):
-			$(this.$minusButtons[i]).removeClass('counter__button_disabled')
+	plus(e) {
+		const i = [...this.$plusButtons].indexOf(e.target);
+		this.values[i]++;
+		this.render();
 	}
-	clear(){
-		this.values.fill(0)
-		localStorage.removeItem(this.localStorageName)
-		this.render()
+	disableOrEnableMinusButton(i) {
+		this.values[i] == 0
+			? $(this.$minusButtons[i]).addClass("counter__button_disabled")
+			: $(this.$minusButtons[i]).removeClass("counter__button_disabled");
 	}
-	apply(){
-		localStorage.setItem(this.localStorageName,JSON.stringify(this.values))
+	clear() {
+		this.values.fill(0);
+		localStorage.removeItem(this.localStorageName);
+		this.render();
 	}
-	modulo(number){
-		if(number==1)
-			return 0
-		if(number>1&&number<5)
-			return 1
-		else
-			return 2
+	apply() {
+		localStorage.setItem(this.localStorageName, JSON.stringify(this.values));
 	}
-	render(){
-		this.$values.each((i,el)=>{
-			$(el).text(this.values[i])
-			this.disableOrEnableMinusButton(i)
-		})
-		this.$display.text(this.displayValue().trim().slice(0,-1))
-			if(this.isGuest){
-				if(this.values.reduce((acc,val)=>acc+val)==0){
-					this.$display.text('Сколько гостей')
+	modulo(number) {
+		if (number == 1) return 0;
+		if (number > 1 && number < 5) return 1;
+		else return 2;
+	}
+	render() {
+		this.$values.each((i, el) => {
+			$(el).text(this.values[i]);
+			this.disableOrEnableMinusButton(i);
+		});
+		this.$display.text(this.displayValue().trim().slice(0, -1));
+		if (this.isGuest) {
+			if (this.values.reduce((acc, val) => acc + val) == 0) {
+				this.$display.text("Сколько гостей");
 			}
 		}
 	}
-	init(){
-		this.$expand.on('click.dropdownExpand',this.expand.bind(this))
-		this.$plusButtons.on('click.plusButton',this.plus.bind(this))
-		this.$minusButtons.on('click.minusButton',this.minus.bind(this))
-		this.$clear.on('click.dropdownClear',this.clear.bind(this))
-		this.$apply.on('click.dropdownApply',this.apply.bind(this))
-		this.render()
+	init() {
+		this.$expand.on("click.dropdownExpand", this.expand.bind(this));
+		this.$plusButtons.on("click.plusButton", this.plus.bind(this));
+		this.$minusButtons.on("click.minusButton", this.minus.bind(this));
+		this.$clear.on("click.dropdownClear", this.clear.bind(this));
+		this.$apply.on("click.dropdownApply", this.apply.bind(this));
+		this.render();
 	}
 }
-$('.js-dropdown').each((i,el)=>{
-	new Dropdown($(el),i)
-})
+$(".js-dropdown").each((i, el) => {
+	new Dropdown($(el), i);
+});
