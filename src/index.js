bf614111d356e9202b5cd1bf0db11135b4f0edb5
tim <timon0596@yandex.ru>
2020-08-25 15:33:58 +0300

    #11

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-function importAll (r) {
+function importAll(r) {
   r.keys().forEach(r);
 }
-import 'jquery'
-import 'jquery-ui/ui/widgets/slider'
-import 'jquery-ui/themes/base/all.css'
-import './index.pug'
-import 'air-datepicker/dist/css/datepicker.min.css'
-import 'bootstrap/js/src/carousel.js'
-import './main.sass'
-require.context('./ui-kit/cards', true, /\.scss$/)
-require.context('./ui-kit', true, /\.sass/)
-require.context('./pages', true, /\.sass/)
-importAll(require.context('./ui-kit', true, /\.js$/))
-importAll(require.context('./pages', true, /\.js$/))
+import "jquery";
+import "jquery-ui/ui/widgets/slider";
+import "jquery-ui/themes/base/all.css";
+import "./index.pug";
+import "air-datepicker/dist/css/datepicker.min.css";
+import "bootstrap/js/src/carousel.js";
+import "./main.sass";
+require.context("./ui-kit/cards", true, /\.scss$/);
+require.context("./ui-kit", true, /\.sass/);
+require.context("./pages", true, /\.sass/);
+importAll(require.context("./ui-kit", true, /\.js$/));
+importAll(require.context("./pages", true, /\.js$/));
 
-const userImg = require.context('./img/users', true, /\.(jpe?g|png)$/)
-const preview_img = require.context('./img/preview', true, /\.(jpe?g|png|svg)$/)
-const imgs = require.context('./img', false, /\.(jpe?g|png|svg)$/)
-
-
+const userImg = require.context("./img/users", true, /\.(jpe?g|png)$/);
+const preview_img = require.context(
+  "./img/preview",
+  true,
+  /\.(jpe?g|png|svg)$/
+);
+const imgs = require.context("./img", false, /\.(jpe?g|png|svg)$/);
--- webpack.config.js
+++ webpack.config.js
@@ -1,125 +1,137 @@
-const path = require('path');
-const HWP=require('html-webpack-plugin');
-const webpack = require('webpack');
-const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
-const MiniCssExtractPlugin=require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
-const fs = require('fs')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-function generateHtmlPlugins (templateDir) {
-	const tpldirpath = path.resolve(__dirname, templateDir)
-	const templateFiles = fs.readdirSync(tpldirpath)
-	return templateFiles.map(item => {
-    
-    	const pugFile = fs.readdirSync(path.resolve(tpldirpath, item)).filter(file=>file.match(/.pug$/))[0]
-    	const parts = pugFile.split('.')
-    	const name = parts[0]
-    	const extension = parts[1]
-    	return new HWP({
-    		filename: `${name}.html`,
-    		template: path.resolve(__dirname, `${path.resolve(tpldirpath, item)}/${name}.${extension}`)
-    	})
-  }).filter(item=>!!item)
+const path = require("path");
+const HWP = require("html-webpack-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const fs = require("fs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+function generateHtmlPlugins(templateDir) {
+  const tpldirpath = path.resolve(__dirname, templateDir);
+  const templateFiles = fs.readdirSync(tpldirpath);
+  return templateFiles
+    .map((item) => {
+      const pugFile = fs
+        .readdirSync(path.resolve(tpldirpath, item))
+        .filter((file) => file.match(/.pug$/))[0];
+      const parts = pugFile.split(".");
+      const name = parts[0];
+      const extension = parts[1];
+      return new HWP({
+        filename: `${name}.html`,
+        template: path.resolve(
+          __dirname,
+          `${path.resolve(tpldirpath, item)}/${name}.${extension}`
+        ),
+      });
+    })
+    .filter((item) => !!item);
 }
-const htmlPlugins = generateHtmlPlugins('./src/pages')
-module.exports={
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname,'dist')
-	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				}
-			}
-		}
-	},
-	module:{
-		rules: [
-		{
-			test:/\.pug$/,
-			loader: 'pug-loader',
-			options: {pretty: true}
-		},
-		{
-			test:/\.js$/,
-			loader: 'babel-loader',
-			exclude:'/node_modules/'
-		},
-		{
-			test: /\.(scss|sass)$/,
-			use: [
-			MiniCssExtractPlugin.loader, 
-			{
-				loader: 'css-loader',
-				options: {
-					sourceMap: true
-				}
-			},
-			{
-				loader: 'postcss-loader',
-				options: {
-					plugins: [
-					autoprefixer()
-					],
-					sourceMap: true
-				}
-			},
-			{
-				loader: 'sass-loader',
-				options: { sourceMap: true }
-			}
-			]
-		},
-		{
-			test:/\.css$/,
-			use:[
-			"style-loader",
-			"css-loader"
-			]
-		},
-		{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-		      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader",exclude: [path.resolve(__dirname, "src/img")], },
-			{
-				test: /\.(jpe?g|png|svg)$/,
-				exclude: [path.resolve(__dirname,"src/fonts")],
-				use:[ {
-					loader: "file-loader",
-					options: {
-						name: "[name].[ext]",
-	          outputPath: (url, resourcePath, context) => {
-	          	if (/preview/.test(resourcePath)) {
-	              return `img/preview/${url}`;
-	            }
-	            if (/users/.test(resourcePath)) {
-	              return `img/users/${url}`;
-	            }
-	            return `img/${url}`;
-	          },
-						useRelativePath: true
-					}}],
-				},]
-			},
-				plugins:[
-				new CleanWebpackPlugin(),
-				new HWP({
-					template: './src/index.pug',
-					filename: 'index.html'
-				}),
-				...htmlPlugins,
-				new webpack.ProvidePlugin({
-					$: 'jquery',
-					jQuery: 'jquery',
-					'window.jQuery': 'jquery'
-				}),
-				new MiniCssExtractPlugin({
-					filename: '[name].[hash].css',
-				}),
-				new HtmlWebpackPugPlugin()
-				]
-			}
+const htmlPlugins = generateHtmlPlugins("./src/pages");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: { pretty: true },
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [autoprefixer()],
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+        exclude: [path.resolve(__dirname, "src/img")],
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        exclude: [path.resolve(__dirname, "src/fonts")],
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: (url, resourcePath, context) => {
+                if (/preview/.test(resourcePath)) {
+                  return `img/preview/${url}`;
+                }
+                if (/users/.test(resourcePath)) {
+                  return `img/users/${url}`;
+                }
+                return `img/${url}`;
+              },
+              useRelativePath: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HWP({
+      template: "./src/index.pug",
+      filename: "index.html",
+    }),
+    ...htmlPlugins,
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+    new HtmlWebpackPugPlugin(),
+  ],
+};
