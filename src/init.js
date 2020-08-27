1bce5db8205dce6c90bd2d1ae98962826e8917db
tim <timon0596@yandex.ru>
2020-08-27 19:33:40 +0300

    #14 sign-in-page oop

--- src/init.js
+++ src/init.js
@@ -1,4 +1,12 @@
 import { RoomDetails } from "./ui-kit/cards/room-details/room-details";
+import { SignInPage } from "./pages/sign-in-page/sign-in-page";
 $(".room-details").each((i, el) => {
   new RoomDetails($(el));
+});
+const cards = {
+  signIn: $(".js-sign-in"),
+  signUp: $(".js-registration"),
+};
+$(document).ready(() => {
+  new SignInPage(cards);
 });
--- src/pages/sign-in-page/sign-in-page.js
+++ src/pages/sign-in-page/sign-in-page.js
@@ -1,27 +1,44 @@
-$(document).ready(() => {
-  const $sign_up = $(".js-registration");
-  const $sign_in = $(".js-sign-in");
-  function signInHandler() {
-    $sign_up.hide();
-    $sign_in.show();
-  }
-  function signUpHandler() {
-    $(".nav__item_unsigned").show();
-    $(".nav__item_signed").hide();
-    $sign_in.hide();
-    $sign_up.show();
-  }
-  function enterHandler() {
-    $(".nav__item_unsigned").hide();
-    $(".nav__item_signed").show();
-  }
-
-  if ("/sign-in-page.html" == location.pathname) {
-    $sign_in.hide();
-    $(".js-registration__sign-in-button,.nav__sign-in-button").click(
-      signInHandler
+export class SignInPage {
+  constructor({ signIn, signUp }) {
+    this.$signUpBlock = signUp;
+    this.$signInBlock = signIn;
+    this.$signUpBlockButton = this.$signUpBlock.find(
+      ".js-registration__sign-in-button"
     );
-    $(".js-sign-in__button-create,.nav__sign-up-button").click(signUpHandler);
-    $(".js-sign-in__sign-in-button").click(enterHandler);
+    this.$signInButtonCreate = this.$signInBlock.find(
+      ".js-sign-in__button-create"
+    );
+    this.$signInEnterButton = this.$signInBlock.find(
+      ".js-sign-in__sign-in-button"
+    );
+    this.$navButtons = $(".nav__item_unsigned");
+    this.$userName = $(".nav__item_signed");
+    this.$navSignUpButton = $(".nav__sign-up-button .button");
+    this.$navSignInButton = $(".nav__sign-in-button .button");
+    this.init();
   }
-});
+  signInHandler() {
+    this.$signUpBlock.hide();
+    this.$signInBlock.show();
+  }
+  signUpHandler() {
+    this.$navButtons.show();
+    this.$userName.hide();
+    this.$signInBlock.hide();
+    this.$signUpBlock.show();
+  }
+  enterHandler() {
+    this.$navButtons.hide();
+    this.$userName.show();
+  }
+  init() {
+    if ("/sign-in-page.html" == location.pathname) {
+      this.$signInBlock.hide();
+      this.$signUpBlockButton.click(this.signInHandler.bind(this));
+      this.$navSignInButton.click(this.signInHandler.bind(this));
+      this.$navSignUpButton.click(this.signUpHandler.bind(this));
+      this.$signInButtonCreate.click(this.signUpHandler.bind(this));
+      this.$signInEnterButton.click(this.enterHandler.bind(this));
+    }
+  }
+}
