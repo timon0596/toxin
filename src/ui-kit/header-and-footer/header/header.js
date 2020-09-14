9f1ecfb4ccbae17f1739e183fb11bbbd6b22b999
timon <timon0596@yandex.ru>
2020-09-14 04:10:59 +0300

    #22 header js corrections

--- src/ui-kit/header-and-footer/header/header.js
+++ src/ui-kit/header-and-footer/header/header.js
@@ -1,13 +1,9 @@
 export class Header {
   constructor() {
-    this.$header;
     this.$navItems;
     this.init();
   }
   init() {
-    this.$header = $("header");
-    this.$navItems = this.$header
-      .find('.nav__item[data-signed="false"]')
-      .hide();
+    this.$navItems = $('[data-signed="false"]').hide();
   }
 }
