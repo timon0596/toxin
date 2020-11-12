2cd94f431f0bc982ea3846f120a56c6be250b1f5
timon <timon0596@yandex.ru>
2020-11-12 13:18:07 +0300

    dropdown rework

--- src/ui-kit/form-elements/dropdown/dropdown.js
+++ src/ui-kit/form-elements/dropdown/dropdown.js
@@ -1,20 +1,10 @@
 export class Dropdown {
-  static declensions = {
-    bed: [
-      ["спальня", "спальни", "спален"],
-      ["кровать", "кровати", "кроватей"],
-      ["ванная", "ванных", "ванных"],
-    ],
-    guest: [
-      ["гость", "гостя", "гостей"],
-      ["младенец", "младенца", "младенцев"],
-    ],
-  };
-
   constructor({
     mainDiv,
     index,
-    counters: { Counter, $minusButtons, $plusButtons, $values },
+    counters: {
+      Counter, $minusButtons, $plusButtons, $values, declension,
+    },
   }) {
     this.index = index;
     this.$mainDiv = mainDiv;
@@ -22,6 +12,7 @@ export class Dropdown {
     this.$minusButtons = $minusButtons;
     this.$plusButtons = $plusButtons;
     this.$values = $values;
+    this.dec = declension;
     this.$display;
     this.$expand;
     this.$expandIcon;
@@ -29,36 +20,22 @@ export class Dropdown {
     this.$menu;
     this.$clear;
     this.$apply;
-    this.isGuest;
-    this.dec;
     this.localStorageName;
-    this.values;
+    this.values = [0, 0, 0];
+    this.type;
     this.init();
   }
 
   findElements() {
-    this.$display = this.$mainDiv.find(".js-dropdown__display");
+    this.$display = this.$mainDiv.find('.js-dropdown__display');
     this.$expand = this.$mainDiv.find(
-      ".js-dropdown__body .js-dropdown__icon-wrapper"
+      '.js-dropdown__body .js-dropdown__icon-wrapper',
     );
-    this.$body = this.$mainDiv.find(".js-dropdown__body");
-    this.$menu = this.$mainDiv.find(".js-dropdown__menu").hide();
-    this.$clear = this.$mainDiv.find(".js-dropdown__clear");
-    this.$apply = this.$mainDiv.find(".js-dropdown__apply");
-    this.isGuest = this.$mainDiv.hasClass("dropdown_guest");
-    this.dec = Dropdown.declensions[this.isGuest ? "guest" : "bed"];
-    this.localStorageName = this.$mainDiv.attr("class").replace(/\s/g, "");
-    this.values = this.localStorageValues();
-  }
-
-  localStorageValues() {
-    const vals = localStorage.getItem(this.localStorageName);
-    return vals
-      ? JSON.parse(vals)
-      : this.$values
-          .text()
-          .split("")
-          .map((n) => +n);
+    this.$body = this.$mainDiv.find('.js-dropdown__body');
+    this.$menu = this.$mainDiv.find('.js-dropdown__menu').hide();
+    this.$clear = this.$mainDiv.find('.js-dropdown__clear');
+    this.$apply = this.$mainDiv.find('.js-dropdown__apply');
+    console.log(this.type = this.$mainDiv.attr('data-type'));
   }
 
   displayValuePart(i) {
@@ -66,38 +43,24 @@ export class Dropdown {
       if (i === 0) {
         return this.values[0] + this.values[1]
           ? `${this.values[0] + this.values[1]} ${
-              this.dec[0][this.modulo((this.values[0] + this.values[1]) % 10)]
-            }, `
-          : "";
-      } else {
-        return this.values[2]
-          ? `${this.values[2]} ${
-              this.dec[1][this.modulo(this.values[2]) % 10]
-            }, `
-          : "";
+            this.dec[0][this.modulo((this.values[0] + this.values[1]) % 10)]
+          }, `
+          : '';
       }
-    } else {
-      return this.values[i]
-        ? `${this.values[i]} ${this.dec[i][this.modulo(this.values[i]) % 10]}, `
-        : "";
+      return this.values[2]
+        ? `${this.values[2]} ${
+          this.dec[1][this.modulo(this.values[2]) % 10]
+        }, `
+        : '';
     }
-  }
-
-  displayValue() {
-    if (this.isGuest) {
-      return `${this.displayValuePart(0)}` + `${this.displayValuePart(2)}`;
-    } else {
-      return (
-        `${this.displayValuePart(0)}` +
-        `${this.displayValuePart(1)}` +
-        `${this.displayValuePart(2)}`
-      );
-    }
+    return this.values[i]
+      ? `${this.values[i]} ${this.dec[i][this.modulo(this.values[i]) % 10]}, `
+      : '';
   }
 
   handleExpandClick() {
     this.$menu.slideToggle(0);
-    this.$body.toggleClass("dropdown__body_active");
+    this.$body.toggleClass('dropdown__body_active');
   }
 
   handleMinusButtonsClick(e) {
@@ -128,33 +91,19 @@ export class Dropdown {
   handleApplyClick() {
     localStorage.setItem(this.localStorageName, JSON.stringify(this.values));
     this.$menu.slideToggle(0);
-    this.$body.toggleClass("dropdown__body_active");
+    this.$body.toggleClass('dropdown__body_active');
   }
 
   modulo(number) {
     if (number === 1) return 0;
     if (number > 1 && number < 5) return 1;
-    else return 2;
-  }
-
-  render() {
-    this.$values.each((i, el) => {
-      $(el).text(this.values[i]);
-      this.disableOrEnableMinusButton(i);
-    });
-    this.$display.text(this.displayValue().trim().slice(0, -1));
-    if (this.isGuest) {
-      if (this.values.reduce((acc, val) => acc + val) === 0) {
-        this.$display.text("Сколько гостей");
-      }
-    }
-    this.sum();
+    return 2;
   }
 
   sum() {
     this.values.reduce((acc, i) => acc + i) === 0
-      ? this.$clear.addClass("dropdown__clear_invis")
-      : this.$clear.removeClass("dropdown__clear_invis");
+      ? this.$clear.addClass('dropdown__clear_invis')
+      : this.$clear.removeClass('dropdown__clear_invis');
   }
 
   binding() {
@@ -168,11 +117,10 @@ export class Dropdown {
   init() {
     this.binding();
     this.findElements();
-    this.$expand.on("click.dropdownExpand", this.handleExpandClick);
-    this.$plusButtons.on("click.plusButton", this.handlePlusButtonsClick);
-    this.$minusButtons.on("click.minusButton", this.handleMinusButtonsClick);
-    this.$clear.on("click.dropdownClear", this.handleClearClick);
-    this.$apply.on("click.dropdownApply", this.handleApplyClick);
-    this.render();
+    this.$expand.on('click.dropdownExpand', this.handleExpandClick);
+    this.$plusButtons.on('click.plusButton', this.handlePlusButtonsClick);
+    this.$minusButtons.on('click.minusButton', this.handleMinusButtonsClick);
+    this.$clear.on('click.dropdownClear', this.handleClearClick);
+    this.$apply.on('click.dropdownApply', this.handleApplyClick);
   }
 }
--- src/ui-kit/form-elements/dropdown/init.js
+++ src/ui-kit/form-elements/dropdown/init.js
@@ -1,16 +1,30 @@
 import { Dropdown } from './dropdown';
 import { Counter } from '../counter/counter';
 
-$('.js-dropdown').each((i, el) => {
-  const $counters = $(el).find('.js-counter');
-  const $minusButtons = $counters.find('.js-counter__button:first-child');
-  const $plusButtons = $counters.find('.js-counter__button:last-child');
-  const $values = $counters.find('.js-counter__button:nth-child(2)');
-  const counters = {
-    Counter: new Counter(),
-    $minusButtons,
-    $plusButtons,
-    $values,
+{
+  const declensions = {
+    rooms: [
+      ['спальня', 'спальни', 'спален'],
+      ['кровать', 'кровати', 'кроватей'],
+      ['ванная', 'ванных', 'ванных'],
+    ],
+    guests: [
+      ['гость', 'гостя', 'гостей'],
+      ['младенец', 'младенца', 'младенцев'],
+    ],
   };
-  new Dropdown({ mainDiv: $(el), index: i, counters });
-});
+  $('.js-dropdown').each((i, el) => {
+    const $counters = $(el).find('.js-counter');
+    const $minusButtons = $counters.find('.js-counter__button:first-child');
+    const $plusButtons = $counters.find('.js-counter__button:last-child');
+    const $values = $counters.find('.js-counter__button:nth-child(2)');
+    const counters = {
+      Counter: new Counter(),
+      $minusButtons,
+      $plusButtons,
+      $values,
+    };
+    new Dropdown({
+      mainDiv: $(el), index: i, counters, declensions,
+    });
+  }); }
