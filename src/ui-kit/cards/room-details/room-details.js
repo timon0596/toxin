ba33c8756f63c840b0bfca3d19f72eb5ee36a02b
timon <timon0596@yandex.ru>
2020-12-01 05:04:57 +0300

    #19

--- src/ui-kit/cards/room-details/room-details.js
+++ src/ui-kit/cards/room-details/room-details.js
@@ -1,5 +1,11 @@
 export class RoomDetails {
-  constructor({ $element, $blockSum }) {
+  constructor({
+    $element, $blockSum, cost = 9990, discount = 2179, tax = 300, currency = '₽',
+  }) {
+    this.currency = currency;
+    this.cost = cost;
+    this.discount = discount;
+    this.tax = tax;
     this.$block = $element;
     this.$blockSum = $blockSum;
     this.init();
@@ -13,16 +19,18 @@ export class RoomDetails {
 
   handleBlockNewDateSelect(e) {
     if (e.selectedDates.days) {
-      const sum = 9990 * e.selectedDates.days;
-      const total = sum - 2179 + 300;
+      const { format } = new Intl.NumberFormat('ru', {});
+
+      const sum = this.cost * e.selectedDates.days;
+      const total = sum - this.discount + this.tax;
       const declension = e.selectedDates.days % 10 === 1 ? 'сутки' : 'суток';
       const taxHabitationInt = Math.floor(sum / 1000);
       const taxHabitationModulo = (`${sum % 1000}`).padStart(3, '0');
-      const taxHabitationTaxSum = `${taxHabitationInt} ${taxHabitationModulo}₽`;
-      const taxHabitationText = `9 990₽ х ${e.selectedDates.days} ${declension}`;
+      const taxHabitationTaxSum = `${taxHabitationInt} ${taxHabitationModulo}${this.currency}`;
+      const taxHabitationText = `${format(this.cost)}${this.currency} х ${e.selectedDates.days} ${declension}`;
       const totalInt = Math.floor(total / 1000);
       const totalModulo = (`${total % 1000}`).padStart(3, '0');
-      const blockSumText = `${totalInt} ${totalModulo}₽`;
+      const blockSumText = `${totalInt} ${totalModulo}${this.currency}`;
       this.$taxHabitation.text(taxHabitationText);
       this.$taxHabitationSumBlock.text(taxHabitationTaxSum);
       this.$blockSum.text(blockSumText);
